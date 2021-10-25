e4f2c887a690729764d72f58e67e60b602952d40
pzhao123 <peterzhao080@gmail.com>
2021-10-25 01:15:55 -0400

    rm unnecessary headers

--- linkedlist.c
+++ linkedlist.c
@@ -1,7 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <string.h>
 #include "linkedlist.h"
 
 struct song_node * insert_front(struct song_node *node, char *artist, char *name){
